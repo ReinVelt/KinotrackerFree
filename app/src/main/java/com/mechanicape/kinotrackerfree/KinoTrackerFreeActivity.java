15a8a6ce3074eaf90a70089528340f17099062b6
velt <velt@dejongduke.nl>
2015-08-05 12:35:08 +0200

    added telephony support (not finished)

--- app/src/main/java/com/mechanicape/kinotrackerfree/KinoTrackerFreeActivity.java
+++ app/src/main/java/com/mechanicape/kinotrackerfree/KinoTrackerFreeActivity.java
@@ -2,11 +2,28 @@ package com.mechanicape.kinotrackerfree;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.mechanicape.kinotrackerfree.R;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class KinoTrackerFreeActivity extends Activity {
 
@@ -57,31 +74,25 @@ public class KinoTrackerFreeActivity extends Activity {
 
     }
 
-    public send()
-    {
 
-        // Defined URL  where to send data
-        URL url = new URL("http://kinotracker.eu/post.php");
 
-        // Send POST data request
-
-        URLConnection conn = url.openConnection();
-        conn.setDoOutput(true);
-        OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-        wr.write( data );
-        wr.flush();
-    }
-
-    public void postData() {
+    public void postData(Location location) {
         // Create a new HttpClient and Post Header
         HttpClient httpclient = new DefaultHttpClient();
         HttpPost httppost = new HttpPost("http://www.yoursite.com/script.php");
+        String uid=this.getIMEI(this);
 
         try {
             // Add your data
             List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
             nameValuePairs.add(new BasicNameValuePair("id", "12345"));
-            nameValuePairs.add(new BasicNameValuePair("stringdata", "Hi"));
+            nameValuePairs.add(new BasicNameValuePair("uid", "Hi"));
+            nameValuePairs.add(new BasicNameValuePair("latitude","hi:"));
+            nameValuePairs.add(new BasicNameValuePair("longitude", "Hi"));
+            nameValuePairs.add(new BasicNameValuePair("speed", "Hi"));
+            nameValuePairs.add(new BasicNameValuePair("heading", "Hi"));
+            nameValuePairs.add(new BasicNameValuePair("timestamp", "Hi"));
+            nameValuePairs.add(new BasicNameValuePair("status", "Hi"));
             httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
             // Execute HTTP Post Request
